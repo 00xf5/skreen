5c3a0751bbd585ffa87ebf644c2417209d58347f
dwight <dwightsyeve49@gmail.com>
2026-05-03 09:32:06 -0700

    fix: Import useEffect in App.jsx

--- controller/src/App.jsx
+++ controller/src/App.jsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import { Routes, Route } from 'react-router-dom'
 import { useWebSocket } from './hooks/useWebSocket'
 import { Dashboard } from './components/Dashboard'
